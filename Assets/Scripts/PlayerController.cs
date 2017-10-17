8f70bb9dbd6918534bd83aaeaa963a737481e66b
Connor McElroy <cmcelroy@ksu.edu>
2017-10-17 16:32:58 -0500

    Got down sprite controls:

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,25 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	// Use this for initialization
-	public int speed;
+
+	public float speed = 1;
+	private Animator animator;
 
 	void Start () {
-		
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		var x = Input.GetAxis ("Horizontal") * 0.1f * speed;
-		var y = Input.GetAxis ("Vertical") * 0.1f * speed;
-	 	
-		if (Input.GetKey("a")) {
-			animation.Play ("Player Left");
-		}
-		transform.Translate (x, y, 0);
+		float horizontal = Input.GetAxis ("Horizontal") * 0.1f * speed;
+		float vertical = Input.GetAxis ("Vertical") * 0.1f * speed;
+
+		if (vertical < 0.0)
+			animator.Play ("Walk Down");
+		Debug.Log (vertical);
+
+		transform.Translate (horizontal, vertical, 0);
+
 	}
 }
