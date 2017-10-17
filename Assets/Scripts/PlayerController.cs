23c829c411a1acdbd75c3c404d8a8c74eacd00ef
Connor McElroy <cmcelroy@ksu.edu>
2017-10-17 14:25:35 -0500

    Added a sprite

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	// Use this for initialization
+	public int speed;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis ("Horizontal") * 0.1f * speed;
+		var y = Input.GetAxis ("Vertical") * 0.1f * speed;
+	 	
+		if (Input.GetKey("a")) {
+			animation.Play ("Player Left");
+		}
+		transform.Translate (x, y, 0);
+	}
+}
